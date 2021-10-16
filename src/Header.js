82ff74bd71b64d6995666e842a1ca24057083763
Luisa Ferreira <luisaaferreira23@gmail.com>
2021-10-16 20:01:45 -0300

    home

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+
+class Header extends Component{
+  render() {
+    return <div className="App">
+           <h1>LM Stocks</h1>
+           </div>
+
+  }
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render () {
+    return <div>
+      <button href="./produtos">Cadastrar produtos</button>
+    </div>
+  }
+}
+
+export default Main;
