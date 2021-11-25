38391b76654eb910e33fed47bec1e04c0093cd35
Luisa Ferreira <luisaaferreira23@gmail.com>
2021-11-25 08:42:59 -0300

    Estilização da homepage

--- src/views/Main.js
+++ src/views/Main.js
@@ -5,7 +5,7 @@ class Main extends Component {
   render () {
     return (
     <div className="homepage">
-      <h1 className="titulo">Hello, Welcome!</h1>
+      <h1 className="title">Hello, Welcome!</h1>
       <p className="descricao">LM Stocks will help you have better inventory control!</p>
     </div>
     )
