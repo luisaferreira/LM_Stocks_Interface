32923351d2b4664042b4347ecef611a6d04e2311
Luisa Ferreira <luisaaferreira23@gmail.com>
2021-11-24 21:52:42 -0300

    Estilização da header

--- src/views/shared/Header.js
+++ src/views/shared/Header.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 
+import logo from '../assets/images/logo.png';
+import '../assets/css/header.css'
 class Header extends Component {
   render() {
     return (
-      <div style={{display: "flex"}}>
-        <h1>LM</h1>
+      <div className="header">
+        <img src={logo} className="logo" />
         {/* estiliza esses navlinks mas não muda por outra tag */}
-        <NavLink activeClassName="active" exact to ="/">Home</NavLink>
-        <NavLink activeClassName="active" exact to ="/products">Products</NavLink>
 
-        <a href="https://github.com/luisaferreira/LM_Stocks_Interface">github</a>
+        <div className="navegacao">
+          <NavLink activeClassName="active" className="links" exact to="/">Home</NavLink>
+          <NavLink activeClassName="active" className="links" exact to="/products">Products</NavLink>
+
+          <a href="https://github.com/luisaferreira/LM_Stocks_Interface" className="links">Github</a>
+        </div>
+
+
       </div>
     )
   }
