500a063f1f8e46c2bf82a3d21a645737272c7d6c
Luisa Ferreira <luisaaferreira23@gmail.com>
2021-11-25 08:59:03 -0300

    Estilização da página de detalhes

--- src/views/ProductDetails.js
+++ src/views/ProductDetails.js
@@ -3,13 +3,15 @@ import Header from "./shared/Header";
 import Footer from "./shared/Footer";
 import axios from "axios";
 
+import './assets/css/productDetails.css'
+
 class ProductDetails extends Component {
-	state = {
+    state = {
         product: {
             id: 0,
             name: "",
             price: 0,
-            weight: 0, 
+            weight: 0,
             quantity: 0,
             lot: "",
             validity: "2020-07-26T00:00:00",
@@ -17,7 +19,7 @@ class ProductDetails extends Component {
         }
     }
 
-	componentDidMount() {
+    componentDidMount() {
         let id = this.props.match.params.id;
 
         if (id) {
@@ -28,23 +30,26 @@ class ProductDetails extends Component {
         }
     }
 
-	render() {
-		this.id = this.props.match.params.id;
-		return (
-			<div>
-				<Header />
-				{/* Essas informações aqui cê ajeita na tela do jeito q quiser, mas lembra de usar o state.product */}
-				<h2>{this.state.product.name}</h2>
-				<p>Lote: {this.state.product.lot}</p>
-				<p>Preço: R$ {this.state.product.price}</p>
-				<p>Quantidade: {this.state.product.quantity}</p>
-				<p>Validade: {this.state.product.validity}</p>
-				<p>Peso: {this.state.product.weight}</p>
-				<p>Descrição: {this.state.product.description}</p>
-				<Footer />
-			</div>
-		)
-	}
+    render() {
+        this.id = this.props.match.params.id;
+        return (
+            <div>
+                <Header />
+                {/* Essas informações aqui cê ajeita na tela do jeito q quiser, mas lembra de usar o state.product */}
+                <main className="container-details">
+                    <h1 className="title-details">Product details</h1>
+                    <h2 className="name-product">{this.state.product.name}</h2>
+                    <p className="details-product"><span className="strong">Lot:</span> {this.state.product.lot}</p>
+                    <p className="details-product"><span className="strong">Price:</span> R$ {this.state.product.price}</p>
+                    <p className="details-product"><span className="strong">Quantity:</span> {this.state.product.quantity} units</p>
+                    <p className="details-product"><span className="strong">Validade:</span> {this.state.product.validity}</p>
+                    <p className="details-product"><span className="strong">Weight:</span> {this.state.product.weight} kg/unit</p>
+                    <p className="details-product"><span className="strong">Description:</span> {this.state.product.description}</p>
+                </main>
+                <Footer />
+            </div>
+        )
+    }
 }
 
-export default ProductDetails; 
+export default ProductDetails;
