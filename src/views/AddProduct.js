74e012a9e65b794d1460c9e37e49c347c9e3bacc
Luisa Ferreira <luisaaferreira23@gmail.com>
2021-11-25 08:10:09 -0300

    Estilização da página de salvar produtos

--- src/views/AddProduct.js
+++ src/views/AddProduct.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import Header from "./shared/Header";
 import Footer from "./shared/Footer";
 import axios from "axios";
-import { useHistory } from "react-router";
+
+import './assets/css/reset.css';
+import './assets/css/addProduct.css';
 
 class AddProduct extends Component {
     state = {
@@ -10,7 +12,7 @@ class AddProduct extends Component {
             id: 0,
             name: "",
             price: 0,
-            weight: 0, 
+            weight: 0,
             quantity: 0,
             lot: "",
             validity: "2020-07-26T00:00:00", //Aqui em validity usa um datetime pra inicialmente setar a validade como a data e hora do momento
@@ -20,13 +22,13 @@ class AddProduct extends Component {
 
     onChange = (e) => {
         let product = this.state.product;
-        
+
         product[e.target.name] = e.target.value;
-        this.setState({product: product});
+        this.setState({ product: product });
     }
 
     save = () => {
-        if(this.state.product.id > 0) {
+        if (this.state.product.id > 0) {
             console.log(123);
 
             axios.put(`https://localhost:5001/products/${this.state.product.id}`, this.state.product).then(response => {
@@ -56,32 +58,36 @@ class AddProduct extends Component {
             <div>
                 {/* Na header dessa página coloca um botão pra poder voltar pra página anterior */}
                 <Header />
-                <h2>{this.id} Details</h2>
-                {/* Se for mudar o input não esquece de colocar o value, name e id */}
-                {/* Tem q validar esse formulário, as instruções tão no documento com teu nome */}
-                <form style={{ display: "flex" },{ flexDirection: "column" }}>
-                    <label htmlFor="name">Name:</label>
-                    <input style={{ display: "block" }} value={this.state.product.name} onChange={this.onChange} name="name" id="name" type="text" placeholder="Name"></input>
-                    
-                    <label htmlFor="price">Price:</label>
-                    <input style={{ display: "block" }} value={this.state.product.price} onChange={this.onChange} name="price" id="price" type="number" placeholder="Price"></input>
-                    
-                    <label htmlFor="weight">Weight:</label>
-                    <input style={{ display: "block" }} value={this.state.product.weight} onChange={this.onChange} name="weight" id="weight" type="number" placeholder="Weight"></input>
-                    
-                    <label htmlFor="quantity">Quantity:</label>
-                    <input style={{ display: "block" }} value={this.state.product.quantity} onChange={this.onChange} name="quantity" id="quantity" type="number" placeholder="Quantity"></input>
-                    
-                    <label htmlFor="lot">Lot:</label>
-                    <input style={{ display: "block" }} value={this.state.product.lot} onChange={this.onChange} name="lot" id="lot" type="text" placeholder="Lot"></input>
-                    
-                    <label htmlFor="validity">Validity:</label>
-                    <input style={{ display: "block" }} value={this.state.product.validity} onChange={this.onChange} name="validity" id="validity" type="datetime-local"></input>
-                    
-                    <label htmlFor="description">Description:</label>
-                    <textarea style={{ display: "block" }} value={this.state.product.description} onChange={this.onChange} name="description" id="description" placeholder="Description"></textarea>
-                    <button onClick={this.save}>Salvar</button>
-                </form>
+                <div className="container">
+                    <h2 className="titulo">Add/update product</h2>
+                    {/* Se for mudar o input não esquece de colocar o value, name e id */}
+                    {/* Tem q validar esse formulário, as instruções tão no documento com teu nome */}
+                    <form className="form">
+                        <label className="labels" htmlFor="name">Name:</label>
+                        <input className="fields" value={this.state.product.name} onChange={this.onChange} name="name" id="name" type="text" placeholder="Name"></input>
+
+                       
+                            <label className="labels" htmlFor="price">Price:</label>
+                            <input className="fields" value={this.state.product.price} onChange={this.onChange} name="price" id="price" type="number" placeholder="Price"></input>
+
+                            <label className="labels" htmlFor="weight">Weight:</label>
+                            <input className="fields" value={this.state.product.weight} onChange={this.onChange} name="weight" id="weight" type="number" placeholder="Weight"></input>
+
+                        
+                        <label className="labels" htmlFor="quantity">Quantity:</label>
+                        <input className="fields" value={this.state.product.quantity} onChange={this.onChange} name="quantity" id="quantity" type="number" placeholder="Quantity"></input>
+
+                        <label className="labels" htmlFor="lot">Lot:</label>
+                        <input className="fields" value={this.state.product.lot} onChange={this.onChange} name="lot" id="lot" type="text" placeholder="Lot"></input>
+
+                        <label className="labels" htmlFor="validity">Validity:</label>
+                        <input className="fields" value={this.state.product.validity} onChange={this.onChange} name="validity" id="validity" type="datetime-local"></input>
+
+                        <label className="labels" htmlFor="description">Description:</label>
+                        <textarea className="fields" value={this.state.product.description} onChange={this.onChange} name="description" id="description" placeholder="Description"></textarea>
+                        <button className="button-save" onClick={this.save}>Save</button>
+                    </form>
+                </div>
                 <Footer />
             </div>
         )
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import './assets/css/main.css'
 
 class Main extends Component {
   render () {
     return (
-    <div>
-      {/* Esse componente eu pensei em colocar o texto explicativo do sistema */}
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas finibus, metus at malesuada molestie, quam nisl pretium neque, a condimentum turpis lectus at mauris. Integer eget hendrerit eros. Donec blandit porttitor vehicula. Aenean mattis ut augue sed fermentum. In hac habitasse platea dictumst. Pellentesque pretium felis ipsum, et facilisis ipsum accumsan ut. Etiam tincidunt libero et orci congue feugiat. Maecenas non est mattis, imperdiet diam nec, ornare mi. Donec congue quis nibh eu tincidunt. Praesent in venenatis nisl. Duis ac mi quis lectus egestas commodo. Sed cursus varius nibh, quis pellentesque lorem semper sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis tortor at massa finibus sagittis.</p>
+    <div className="homepage">
+      <h1 className="titulo">Hello, Welcome!</h1>
+      <p className="descricao">LM Stocks will help you have better inventory control!</p>
     </div>
     )
   }
